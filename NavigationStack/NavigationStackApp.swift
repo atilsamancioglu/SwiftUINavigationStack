e0d6c1baa62bfbe9363518c509d0dc6c3d7d38bd
Atıl Samancıoğlu <samancioglu.atil@gmail.com>
2022-08-06 16:14:26 +0300

    Initial Commit

---
+++ NavigationStack/NavigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackApp.swift
+//  NavigationStack
+//
+//  Created by Atil Samancioglu on 6.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
